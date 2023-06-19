d017961c3b79ca5c3423d47b608ea2a75a0c9312
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-19 03:07:32 -0700

    Task 4 and 5

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,9 @@
 -- Create database, create user, add user to db with Select
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db TO 'hbnb_test'@'localhost';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
