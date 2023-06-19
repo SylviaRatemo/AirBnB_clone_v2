7f0cb7c344237f95328a2ef00bbb5e6c1bb93b3c
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-19 03:03:10 -0700

    Task 4 and 5

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,5 @@
 -- Create database, create user, add user to db with Select
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
