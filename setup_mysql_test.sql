de0c9a59b454d4a22e88149c885f1565055ac6cc
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-19 03:13:01 -0700

    SQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,7 @@
 -- Create database, create user, add user to db with Select
+-- DB = hbnb_dev_dv
+-- User = hbnb_dev
+-- Password = hbnb_pwd
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
