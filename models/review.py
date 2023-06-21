b011ef09f41d035472e5b51d27d58b90ba271d4d
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-21 14:40:29 +0300

    AirBnB v2

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 
