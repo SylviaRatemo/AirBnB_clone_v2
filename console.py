6ec21f2324dd0a5bf68c230328ed1b1fa6f4bdf5
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-21 12:30:00 +0300

    task 6

--- console.py
+++ console.py
@@ -224,7 +224,7 @@ class HBNBCommand(cmd.Cmd):
             return
         try:
             args = line.split(" ")
-            if args[0] not in self.__classes:
+            if args[0] not in self.classes:
                 raise NameError()
 
             obj = storage.all(eval(args[0]))
