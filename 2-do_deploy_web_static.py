c86068a90b3c99870ba05e7f0405289296c2fdb7
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-10 02:31:34 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,49 @@
 #!/usr/bin/python3
-""" Deployment"""
+"""Deployemt"""
 
+from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ['100.25.10.249', '34.232.53.87']
+env.hosts = ["100.25.10.249", "34.232.53.87"]
 env.user = "ubuntu"
 
 
+def do_pack():
+    """Archive path """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    location = "versions/web_static_{}.tgz".format(date)
+    archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if archive.succeeded:
+        return location
+    else:
+        return None
+
+
 def do_deploy(archive_path):
-    """ Deploys archive"""
-    if not os.path.exists(archive_path):
-        return False
+    """ Distribute """
 
-    results = []
+    if os.path.exists(archive_path):
 
-    res = put(archive_path, "/tmp")
-    results.append(res.succeeded)
+        archived_file = archive_path[9:]
 
-    basename = os.path.basename(archive_path)
-    if basename[-4:] == ".tgz":
-        name = basename[:-4]
-    newdir = "/data/web_static/releases/" + name
-    run("mkdir -p " + newdir)
-    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
 
-    run("rm /tmp/" + basename)
-    run("mv " + newdir + "/web_static/* " + newdir)
-    run("rm -rf " + newdir + "/web_static")
-    run("rm -rf /data/web_static/current")
-    run("ln -s " + newdir + " /data/web_static/current")
+        print("New version deployed!")
+        return True
 
-    print("New version deployed!")
-    return True
+    return False
