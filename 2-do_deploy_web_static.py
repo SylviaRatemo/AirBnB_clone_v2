e518d325ff43cf90f913c9e66af1c6167ab7e251
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-20 03:49:44 -0700

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,37 @@
 #!/usr/bin/python3
-"""Deployemt"""
+""" Deployment"""
 
-from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["100.25.10.249", "34.232.53.87"]
+env.hosts = ["34.232.53.87", "100.25.10.249"]
 env.user = "ubuntu"
 
 
-def do_pack():
-    """Archive path """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    location = "versions/web_static_{}.tgz".format(date)
-    archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if archive.succeeded:
-        return location
-    else:
-        return None
-
-
 def do_deploy(archive_path):
-    """ Distribute """
+    """ Deploys archive to servers"""
+    if not os.path.exists(archive_path):
+        return False
 
-    if os.path.exists(archive_path):
+    results = []
 
-        archived_file = archive_path[9:]
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
 
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("sudo mkdir -p " + newdir)
+    run("sudo tar -xzf /tmp/" + basename + " -C " + newdir)
 
-        print("New version deployed!")
-        return True
+    run("sudo rm /tmp/" + basename)
 
-    return False
+    run("sudo rsync -a /data/web_static/releases/{}/web_static/ {}/".format(name, newdir))
+    run("sudo rm -rf " + newdir + "/web_static")
+
+    current_path = "/data/web_static/current"
+    run("sudo rm -rf {}".format(current_path))
+    run("sudo ln -s {} {}".format(newdir, current_path))
+
+    return True
