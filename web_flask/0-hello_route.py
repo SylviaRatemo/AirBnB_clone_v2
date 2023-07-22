1a98f9cdd4a98de88e9449baa4a35b0b4b20d039
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-22 10:39:03 -0700

    Hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
-from flask import Flask, request
-from markupsafe import escape
+#!/usr/bin/python3
+"""Flask app"""
 
+from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
-    name = request.args.get("name", "HBNB")
-    return f'Hello, {escape(name)}!'
+    return f'Hello, HBNB!'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
